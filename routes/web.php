54bce49ee8b41cf922a23ad24de1e01f6d2763ba
fray david herrera duarte <148037427+Frayherrera@users.noreply.github.com>
2025-05-14 17:46:14 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -46,13 +46,10 @@ Route::middleware(['auth'])->group(function () {
 });
 
 // Redirección después de login
-Route::get('/home', [HomeController::class, 'redirect'])->name('home.redirect');
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-// productois
+// productos
 Route::get('/home', [ProductController::class, 'index'])->name('productos.index');
 Route::get('/productos/{id}', [ProductController::class, 'show'])->name('productos.show');
 Route::get('/categoria/{slug}', [ProductController::class, 'porCategoria'])->name('productos.categoria');
@@ -60,7 +57,6 @@ Route::get('/categoria/{slug}', [ProductController::class, 'porCategoria'])->nam
 //carrito de compras
 use App\Http\Controllers\CarritoController;
 use App\Http\Controllers\PedidosController;
-
 Route::get('/carrito', [CarritoController::class, 'index'])->name('carrito.index');
 Route::post('/carrito/agregar', [CarritoController::class, 'agregar'])->name('carrito.agregar');
 Route::post('/carrito/actualizar', [CarritoController::class, 'actualizar'])->name('carrito.actualizar');
